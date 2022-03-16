8b5061243eda288d34609fe08de8ffa7eec10f25
Cibbi <12004047+Cibbi@users.noreply.github.com>
2022-03-16 20:25:28 +0100

    Test release

--- .github/prepare-package.js
+++ .github/prepare-package.js
@@ -1,36 +1,7 @@
 ﻿const fs = require('fs-extra');
 const yaml = require('js-yaml');
 const path = require('path');
-
-/*let folder = "Editor"
-if (!process.argv[2]) {
-    console.log("a version number is needed");
-    return;
-}
-
-if (process.argv[3]) {
-    folder = process.argv[3];
-}
-*/
-function fromDir(startPath,filter,callback){
-
-    //console.log('Starting from dir '+startPath+'/');
-
-    if (!fs.existsSync(startPath)){
-        console.log("no dir ",startPath);
-        return;
-    }
-
-    var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        var filename=path.join(startPath,files[i]);
-        var stat = fs.lstatSync(filename);
-        if (stat.isDirectory()){
-            fromDir(filename,filter,callback); //recurse
-        }
-        else if (filter.test(filename)) callback(filename);
-    }
-}
+const exec = require('child_process').execSync;
 
 try {
 
@@ -40,10 +11,12 @@ try {
     else
     {
         console.log("Extra folders found");
-        let fileContents = fs.readFileSync('.github/package-extra-folders.yml', 'utf8');
+        let fileContents = fs.readFileSync('.github/package-info.yml', 'utf8');
         let data = yaml.load(fileContents);
 
         console.log("   Using folder: " + data['destination-folder']);
+        exec('echo ::set-output name=PACKAGE_NAME::' + data['package-name']);
+        exec('echo ::set-output name=VISUAL_NAME::' + data['visual-name']);
 
         var files=fs.readdirSync("tmp");
 
